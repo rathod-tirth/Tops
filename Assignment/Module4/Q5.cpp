1bbaf5f3ce0d9babeeea730e6163a47379c78d24
rathod-tirth <tirthrathod6@gmail.com>
2023-03-04 10:28:42 +0530

    Assignment

--- Assignment/Module4/Q5.cpp
+++ Assignment/Module4/Q5.cpp
@@ -7,25 +7,66 @@ class A
 {
     public:
 
-    string name;
-    int num, amo, type;
+    string name, type;
+    int num, amo, choice, depo, with;
 
     void get()
     {
-        cout<<"\n\n----------Money Bank-----------\n\n";
+        name="Tirth";
+        type="Saving";
+        num=32468447;
+        amo=1000000;
+        // cout<<"\n\n----------Money Bank-----------\n\n";
         
-        cout<<"Name of the Depositor : ";
-        cin>>name;
+        // cout<<"Enter Name of the Depositor : ";
+        // cin>>name;
 
-        cout<<"Account no. : ";
-        cin>>num;
+        // cout<<"Enter Account no. : ";
+        // cin>>num;
+
+        // cout<<"\nType of Account : \n1. Saving Account\n2. Current Account\n3. Salary Account\n\nEnter : ";
+        // cin>>type;
+
+        // cout<<"Enter Total Balance in Account : ";
+        // cin>>amo;
+    }
+
+    void set()
+    {
+        cout<<"\n\n1. To Deposit an amount\n2. To check balance\n3. To Withdraw\nEnter your choice :";
+        cin>>choice;
+
+        switch (choice)
+        {
+        case 1:
+            cout<<"\nEnter amount you want to deposit : ";
+            cin>>depo;
+            break;
+        
+        case 2:
+            cout<<endl<<name<<endl;
+            cout<<"Your Balance : "<<amo;
+            break;
+
+        case 3:
+            cout<<"\n Your Balance : \n"<<amo;
+            cout<<"How Much would you like to withdraw : ";
+            cin>>with;
+            break;
+        
+        default:
+            cout<<"\nWrong Input\n";
+            break;
+        }
     }
 };
 
 int main()
 {
     A obj;
+
     obj.get();
+    obj.set();
 
     return 0;
 }
