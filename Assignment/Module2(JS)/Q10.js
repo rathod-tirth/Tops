15c7d1067ef4113de582add8c63757ee6325dc5c
rathod-tirth <tirthrathod6@gmail.com>
2023-05-24 14:40:02 +0530

    Assignment

---
+++ Assignment/Module2(JS)/Q10.js
@@ -0,0 +1,35 @@
+var fs = require("fs");
+console.log(" Writing into an file ");
+
+// Sample.txt is an empty file
+fs.writeFile(
+    "sample.txt",
+    "Let's write a few sentences in the file",
+    function (err) {
+        if (err) {
+            return console.error(err);
+        }
+
+        // If no error the remaining code executes
+        console.log(" Finished writing ");
+        console.log("Reading the data that's written");
+
+        // Reading the file
+        fs.readFile("sample.txt", function (err, data) {
+            if (err) {
+                return console.error(err);
+            }
+            console.log("Data read : " + data.toString());
+
+        });
+    }
+);
+// use node.js to run the code
+
+/*  On the client side, you can’t read or write files in JavaScript browsers. The fs module in Node.js may 
+be used to accomplish this on the server-side. It has methods for reading and writing files on the file 
+system that are both synchronous and asynchronous. 
+
+    The fs.readFile() and rs.writeFile() methods are used to read and write of a file using javascript. 
+The file is read using the fs.readFile() function, which is an inbuilt method. This technique reads the 
+full file into memory and stores it in a buffer. */
