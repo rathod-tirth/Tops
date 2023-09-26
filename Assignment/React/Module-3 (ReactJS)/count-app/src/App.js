c14643a5279ad0671c3077bdaa5fdd401c2d0a0c
rathod-tirth <tirthrathod6@gmail.com>
2023-09-26 20:07:59 +0530

    complete

--- Assignment/React/Module-3 (ReactJS)/count-app/src/App.js
+++ Assignment/React/Module-3 (ReactJS)/count-app/src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
-    
+      <div className="container">
+        <h2>React Count</h2>
+        <span>{count}</span>
+        <div className="wrapper">
+          <button className="btn" onClick={() => setCount(count + 1)}>Increment</button>
+          <button className="btn" onClick={() => setCount(count + 1)}>Decrement</button>
+        </div>
+        <button className="btn" onClick={() => setCount(0)}>Reset</button>
+      </div>
     </>
   );
 }
