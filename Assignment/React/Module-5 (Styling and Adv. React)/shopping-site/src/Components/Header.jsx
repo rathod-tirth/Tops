e5171bb3a0261e268a93095c95ce7ca376971cda
rathod-tirth <tirthrathod6@gmail.com>
2023-10-05 18:36:11 +0530

    add header

--- Assignment/React/Module-5 (Styling and Adv. React)/shopping-site/src/Components/Header.jsx
+++ Assignment/React/Module-5 (Styling and Adv. React)/shopping-site/src/Components/Header.jsx
@@ -1,28 +1,70 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 function Header() {
    return (
       <>
-         <nav>
-            <Wrapper>
-               <Title>Fruits</Title>
-            </Wrapper>
-            <Wrapper>
-               <ul>
-                  <li>Home</li>
-                  <li>About</li>
-                  <li>Pages</li>
-                  <li>News</li>
-                  <li>Contact</li>
-                  <li>Shop</li>
-               </ul>
-            </Wrapper>
-         </nav>
+         <header>
+            <Nav>
+               <section>
+                  <Logo href="#">Fruits.com</Logo>
+               </section>
+               <section>
+                  <ul>
+                     <List>Home</List>
+                     <List>About</List>
+                     <List>Pages</List>
+                     <List>News</List>
+                     <List>Contact</List>
+                     <List>Shop</List>
+                  </ul>
+               </section>
+               <section>
+                  <Icon><i className="fa-solid fa-cart-shopping" /></Icon>
+                  <Icon><i className="fa-solid fa-magnifying-glass" /></Icon>
+               </section>
+            </Nav>
+         </header>
       </>
    )
 }
 export default Header
 
-const Wrapper = styled.section`
+const Nav = styled.nav`
+   display: flex;
+   width: 100vw;
+   height: 80px;
+   background-color: #051922;
+   color: white;
+   justify-content: space-around;
+   align-items: center;
+`;
 
+const Logo = styled.a`
+   text-decoration: none;
+   color: orange;
+   font-size: 1.5rem;
+   font-weight: 700;
+`;
+
+const List = styled.li`
+   list-style: none;
+   display: inline;
+   font-size: 0.8rem;
+   font-weight: 700;
+   margin: 0 0.5rem;
+   cursor: pointer;
+   transition: 0.3s ease;
+
+   &:hover{
+      color: orange;
+   }
+`;
+
+const Icon = styled.span`
+   margin: 0 0.5rem;
+   cursor: pointer;
+
+   &:hover{
+      color: orange;
+   }
 `;
--- Assignment/React/Module-5 (Styling and Adv. React)/shopping-site/src/index.js
+++ Assignment/React/Module-5 (Styling and Adv. React)/shopping-site/src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <GlobalStyle />
   </React.StrictMode>
 );
