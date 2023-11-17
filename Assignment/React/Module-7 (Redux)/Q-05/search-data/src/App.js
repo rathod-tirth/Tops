61accac0b4ee7d4cd6ad66f57b11b76bae9e89e9
rathod-tirth <tirthrathod6@gmail.com>
2023-11-17 23:12:51 +0530

    add comment

--- Assignment/React/Module-7 (Redux)/Q-05/search-data/src/App.js
+++ Assignment/React/Module-7 (Redux)/Q-05/search-data/src/App.js
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+// importing neccessary dependencies
 
 function App() {
+  // fetching the data on initial render
   useEffect(() => { fetch() }, []);
 
+  // for storing the api data
   const [data, setData] = useState([]);
 
+  // function for fectching the data
   const fetch = async () => {
     try {
       const res = await axios.get("http://localhost:3000/product");
@@ -18,14 +22,17 @@ function App() {
 
   const [value, setValue] = useState();
 
+  // onChange
   const handleChange = (e) => {
     setValue(e.target.value);
     fetch();
   };
 
+  // search bar
   const handleSearch = (e) => {
     e.preventDefault();
     if (value) {
+      // filtering the data
       const newData = data.filter(val => val.name.includes(value));
       setData(newData);
     }
@@ -34,8 +41,10 @@ function App() {
   return (
     <>
       <div className="container m-5">
+        {/* heading */}
         <h2>Search Data</h2>
         <br />
+        {/* search-bar */}
         <div>
           <button className="float-left btn btn-primary m-1" onClick={fetch}><i className="fa-solid fa-rotate"></i></button>
           <form className="form-inline float-right m-2">
@@ -43,7 +52,9 @@ function App() {
             <button type="submit" className="btn btn-primary m-1" onClick={(e) => handleSearch(e)}><i className="fa-solid fa-magnifying-glass"></i></button>
           </form>
         </div>
+        {/* search-bar end */}
 
+        {/* table */}
         <table className="table table-hover table-bordered">
           <thead>
             <tr>
@@ -54,6 +65,7 @@ function App() {
             </tr>
           </thead>
           <tbody>
+            {/* mapping over the api data */}
             {data.map((pro) => {
               return (
                 <tr key={pro.id}>
