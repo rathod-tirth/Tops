3ff783596cff4a2a6d38fdaf68cd5a131e4c2f17
rathod-tirth <tirthrathod6@gmail.com>
2023-11-18 16:41:35 +0530

    add comment

--- Assignment/React/Module-7 (Redux)/Q-06/redux-crud/src/Feature/userReducer.js
+++ Assignment/React/Module-7 (Redux)/Q-06/redux-crud/src/Feature/userReducer.js
@@ -1,15 +1,20 @@
 import axios from "axios";
 
+// action types
 const ALLDATA = "ALLDATA";
 const SINGLEDATA = "SINGLEDATA";
 
+// creating object for our reducer
 const userObject = {
    allUser: [],
    singleUser: [],
    name: "Tirth Rathod"
 };
 
+// reducer
 const userReducer = (state = userObject, action) => {
+
+   // condition over which type is called and performing the action
    switch (action.type) {
       case ALLDATA:
          return {
@@ -28,8 +33,11 @@ const userReducer = (state = userObject, action) => {
 
 export default userReducer;
 
+// setting up the payload data for ALLDATA
 const getData = (data) => ({ type: ALLDATA, payload: data });
 
+// creating a reusable function for fetching all the data
+// which will also set the data to our reducer function
 export const fetchUser = (api) => {
    return function (dispatch) {
       axios.get(api)
@@ -37,8 +45,10 @@ export const fetchUser = (api) => {
    }
 }
 
+// setting up the payload data for SINGLEDATA
 const getSingleData = (data) => ({ type: SINGLEDATA, payload: data });
 
+// creating a reusable function for fetching single user data
 export const fetchSingleUser = (api) => {
    return function (dispatch) {
       axios.get(api)
