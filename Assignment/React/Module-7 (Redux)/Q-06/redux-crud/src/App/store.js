409df0eab77c87987bf4be4db1ea352680a08136
rathod-tirth <tirthrathod6@gmail.com>
2023-11-18 16:42:09 +0530

    add comment

--- Assignment/React/Module-7 (Redux)/Q-06/redux-crud/src/App/store.js
+++ Assignment/React/Module-7 (Redux)/Q-06/redux-crud/src/App/store.js
@@ -4,6 +4,8 @@ import rootReducer from "./rootReducer";
 
 const middleWare = [reduxThunk]
 
+// creating store with the rootReducer we imported
+// applying reduxThunk with applyMiddleware, which will help in async operation througout the app
 const store = createStore(rootReducer, applyMiddleware(...middleWare));
 
 export default store;
