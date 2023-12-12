4c982766ae90cf2599ea988c554e24021aa0e333
rathod-tirth <tirthrathod6@gmail.com>
2023-12-12 16:41:32 +0530

    Assignment

---
+++ Assignment/Python/Module-2 (Fundamentals of python)/Q-07.py
@@ -0,0 +1,8 @@
+# Q-07 Write a Python program to find whether a given number is even or odd, print out an appropriate message to the user.
+
+num=int(input("Enter num: "))
+
+if(num%2==0):
+    print("Even Number")
+else:
+    print("Odd Number")
