4577730cdeb1c995df48c912520114a67cbda2d3
rathod-tirth <tirthrathod6@gmail.com>
2023-12-12 16:19:28 +0530

    complete

---
+++ Assignment/Python/Module-2 (Fundamentals of python)/Q-01.py
@@ -0,0 +1,10 @@
+# Q-01: Write a Python program to check if a number is positive, negative or zero.
+
+num=int(input("Enter num: "))
+
+if(num>0):
+    print("Number is positive")
+elif(num<0):
+    print("Number is negetive")
+elif(num==0):
+    print("Number is zero")
