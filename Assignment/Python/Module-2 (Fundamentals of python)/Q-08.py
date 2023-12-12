cfe95c6053e17e873c273940db78a9cca6e04226
rathod-tirth <tirthrathod6@gmail.com>
2023-12-12 16:41:57 +0530

    Assignment

---
+++ Assignment/Python/Module-2 (Fundamentals of python)/Q-08.py
@@ -0,0 +1,8 @@
+# Q-08 Write a Python program to test whether a passed letter is a vowel or not.
+
+char=input("Enter char: ")
+
+if char=='a' or char=='e' or char=='i' or char=='o' or char=='u' or char=='A' or char=='E' or char=='I' or char=='O' or char=='U':
+    print("Character is vowel")
+else:
+    print("Character is consonant")
