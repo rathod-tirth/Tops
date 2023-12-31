7b726c73e4f598059bc4feaca2365149e29ed41d
rathod-tirth <tirthrathod6@gmail.com>
2023-12-31 12:19:04 +0530

    update comment

--- Assignment/Python/Module–3 (Collections, functions and Modules)/Q-33.py
+++ Assignment/Python/Module–3 (Collections, functions and Modules)/Q-33.py
@@ -12,3 +12,12 @@ desc=dict(sorted(di.items(), key=lambda x:x[1], reverse=True))
 print("Ascending Order : ",asc)
 print("Descending Order : ",desc)
 
+'''
+>> Sorted:
+   - sorted(list, key=..., reverse=...)
+   - key: accepts a function that takes an individual element from the iterable and returns a sorting criteria.
+   
+>> Here key is used to custom sort, where we call an lambda function to shorten the code.
+>> "x" access the individual element of dict which is tuple
+>> since its a tuple x[1] represents the values() of the dict and that is our sorting criteria
+'''
