6e80c3deaf50b92fb2feb08191f03d0c16fb3437
rathod-tirth <tirthrathod6@gmail.com>
2023-12-31 12:26:24 +0530

    Q-2 and Q-3 combine

--- Assignment/Python/Module–3 (Collections, functions and Modules)/Q-02.py
+++ Assignment/Python/Module–3 (Collections, functions and Modules)/Q-02.py
@@ -1,4 +1,4 @@
-# Q-02 How will you remove last object from a list?
+# Q-02 a) How will you remove last object from a list?
 
 # pop() method: removes the last element from the list
 li = [1, 2, 3, 4, 5]
@@ -9,3 +9,8 @@ print(li)
 li = [1, 2, 3, 4, 5]
 li = li[:-1]
 print(li)
+
+# b) Suppose list1 is [2, 33, 222, 14, and 25], what is list1 [-1]?
+
+list1=[2,33,222,14,25]
+print(list1[-1]) # output: 25
