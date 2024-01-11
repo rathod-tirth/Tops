f15b1ecadaed9beff620542faac6f0bf13f4fb02
rathod-tirth <tirthrathod6@gmail.com>
2024-01-11 13:21:49 +0530

    add Q-03

---
+++ Assignment/Python/Module-4 (Advanced python programming)/Q-03.py
@@ -0,0 +1,12 @@
+# Q-03 Write a Python program to append text to a file and display the text.
+
+file=open('Q-03.txt','a+')
+
+file.write("How are you doing?\n")
+file.write("Have a nice day.")
+
+file.seek(0)
+
+print(file.read())
+
+file.close()
