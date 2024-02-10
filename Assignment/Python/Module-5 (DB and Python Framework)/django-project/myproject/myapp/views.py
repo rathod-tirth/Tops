1478d3d3fac4a2880710967b1b35d642898c21b1
rathod-tirth <tirthrathod6@gmail.com>
2024-02-10 09:36:25 +0530

    add home view

--- Assignment/Python/Module-5 (DB and Python Framework)/django-project/myproject/myapp/views.py
+++ Assignment/Python/Module-5 (DB and Python Framework)/django-project/myproject/myapp/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def home(request):
+   return HttpResponse("<h2>Hello From Django<h2>")
