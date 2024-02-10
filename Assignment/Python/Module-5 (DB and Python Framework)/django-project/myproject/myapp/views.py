2b9147d1ba3ce5c209915ea84a219b432eaf9056
rathod-tirth <tirthrathod6@gmail.com>
2024-02-10 12:27:08 +0530

    add comments

--- Assignment/Python/Module-5 (DB and Python Framework)/django-project/myproject/myapp/views.py
+++ Assignment/Python/Module-5 (DB and Python Framework)/django-project/myproject/myapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .models import *
 
 # Create your views here.
+
+# default view
 def home(request):
    if 'name' in request.session:
       user=User.objects.get(name=request.session['name'])
@@ -10,12 +12,15 @@ def home(request):
    else:
       return render(request, 'myapp/login.html')
 
+# login view
 def login(request):
+   # if already logged in
    if 'name' in request.session:
       user=User.objects.get(name=request.session['name'])
       context={'user':user}
       return render(request, 'myapp/index.html',context)
-   else:   
+   else:
+      # login in the user
       if request.POST:
          try:
             user=User.objects.get(name=request.POST.get('name'))
@@ -29,10 +34,12 @@ def login(request):
             return render(request, 'myapp/login.html',{'msg':msg})
       else:
          return render(request, 'myapp/login.html')
-      
+
+# Add product
 def addpro(request):
    if 'name' in request.session:
       if request.POST:
+         # creating new objects for new products
          productmst=ProductMst.objects.create(product_name=request.POST.get('product_name'))
          
          if productmst:
@@ -46,15 +53,15 @@ def addpro(request):
             
             context={'msg':"Product Added Successfully"}
             return render(request, 'myapp/addpro.html', context)
-      else:
-         user=User.objects.get(name=request.session['name'])
-         context={'user':user}
-         return render(request, 'myapp/addpro.html', context)
+      
+      return render(request, 'myapp/addpro.html')
    else:
       return redirect('login')
-   
+
+# view product
 def viewpro(request):
    if 'name' in request.session:
+      # getting data
       user=User.objects.get(name=request.session['name'])
       productsubcat=ProductSubCat.objects.all()
       context={'productsubcat':productsubcat,
@@ -63,8 +70,10 @@ def viewpro(request):
       return render(request, 'myapp/viewpro.html', context)
    return redirect('login')
 
+# edit product
 def editpro(request,k):
    if 'name' in request.session:
+      # getting data
       productmst=ProductMst.objects.get(product_id=k)
       productsubcat=ProductSubCat.objects.get(product_name=productmst.product_id)
       context={'productmst':productmst,
@@ -72,6 +81,7 @@ def editpro(request,k):
       
       print("=========== Product :",productmst,productsubcat)
       
+      # updating data
       if request.POST:
          productmst.product_name=request.POST.get('product_name') or productmst.product_name
          productsubcat.product_price=request.POST.get('product_price') or productsubcat.product_price
@@ -79,6 +89,7 @@ def editpro(request,k):
          productsubcat.model=request.POST.get('model') or productsubcat.model
          productsubcat.ram=request.POST.get('ram') or productsubcat.ram
          
+         # saving data
          productmst.save()
          productsubcat.save()
          return redirect('viewpro')
@@ -86,15 +97,19 @@ def editpro(request,k):
       return render(request, 'myapp/editpro.html', context)
    return redirect('login')
 
+# delete product
 def deletepro(request,k):
    if 'name' in request.session:
+      # getting data
       productmst=ProductMst.objects.get(product_id=k)
+      # deleting data
       productmst.delete()
       
       return redirect('viewpro')
    else:
       return redirect('login')
 
+# logout view
 def logout(request):
    if 'name' in request.session:
       del request.session['name']
