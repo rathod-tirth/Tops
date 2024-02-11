7495a40891c845e84d923b563fe59d2ef346ac0c
rathod-tirth <tirthrathod6@gmail.com>
2024-02-11 16:43:17 +0530

    add book model

--- Assignment/Python/Module-6 (Rest Framework)/myproject/myapp/models.py
+++ Assignment/Python/Module-6 (Rest Framework)/myproject/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+   title=models.CharField(max_length=30)
+   author=models.CharField(max_length=20)
+   isbn=models.CharField(max_length=13, unique=True)
+   publisher=models.CharField(max_length=50)
+   
+   def __str__(self):
+       return self.title
+   
